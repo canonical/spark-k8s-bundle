ff6074c0ae9bfa75a99b2a136f6df78a05bfbf05
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-05-02 11:26:28 +0200

    fix linting

--- python/tests/unittest/test_vanilla.py
+++ python/tests/unittest/test_vanilla.py
@@ -2,8 +2,8 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+
 def test_imports():
     from spark_test import BINS
-
     from spark_test.core.pod import Pod
     from spark_test.core.s3 import Bucket
