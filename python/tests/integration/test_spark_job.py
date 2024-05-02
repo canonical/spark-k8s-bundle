f42d2222390bf27d43c173d368de7eb790757f72
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-05-02 17:01:59 +0000

    wip

--- python/tests/integration/test_spark_job.py
+++ python/tests/integration/test_spark_job.py
@@ -23,6 +23,8 @@ from spark_test.utils import get_spark_drivers
 from .helpers import (
     Bundle,
     deploy_bundle,
+    deploy_bundle_terraform,
+    deploy_bundle_yaml,
     generate_tmp_file,
     get_secret_data,
     list_s3_objects,
@@ -59,58 +61,6 @@ def namespace(namespace_name):
     return namespace_name
 
 
-@pytest_asyncio.fixture(scope="module")
-async def cos(ops_test: OpsTest, cos_model):
-    if cos_model and cos_model not in ops_test.models:
-
-        base_url = (
-            "https://raw.githubusercontent.com/canonical/cos-lite-bundle/main/overlays"
-        )
-
-        overlays = ["offers-overlay.yaml", "testing-overlay.yaml"]
-
-        def create_file(path: Path, response: requests.Response):
-            path.write_text(response.content.decode("utf-8"))
-            return path
-
-        with local_tmp_folder("tmp-cos") as tmp_folder:
-            logger.info(tmp_folder)
-
-            cos_bundle = Bundle[str | Path](
-                main="cos-lite",
-                overlays=[
-                    create_file(tmp_folder / overlay, response)
-                    for overlay in overlays
-                    if (response := requests.get(f"{base_url}/{overlay}"))
-                    if response.status_code == 200
-                ],
-            )
-
-            await ops_test.track_model(COS_ALIAS, model_name=cos_model)
-
-            with ops_test.model_context(COS_ALIAS) as model:
-
-                retcode, stdout, stderr = await deploy_bundle(ops_test, cos_bundle)
-                assert retcode == 0, f"Deploy failed: {(stderr or stdout).strip()}"
-                logger.info(stdout)
-
-                await model.create_offer("traefik:ingress")
-
-            time.sleep(15)
-
-        yield cos_model
-
-        await ops_test.forget_model(cos_model)
-    else:
-        if cos_model:
-            await ops_test.track_model(COS_ALIAS, model_name=cos_model)
-
-        yield cos_model
-
-        if cos_model:
-            await ops_test.forget_model(cos_model)
-
-
 @pytest.mark.abort_on_fail
 @pytest.mark.asyncio
 async def test_deploy_bundle(
@@ -118,53 +68,27 @@ async def test_deploy_bundle(
 ):
     """Deploy the bundle."""
 
-    data = {
-        "namespace": service_account.namespace,
-        "service_account": service_account.name,
-        "bucket": bucket.bucket_name,
-        "s3_endpoint": bucket.s3.meta.endpoint_url,
-    } | ({"cos_controller": ops_test.controller_name, "cos_model": cos} if cos else {})
-
-    with local_tmp_folder("tmp") as tmp_folder:
-        logger.info(tmp_folder)
-
-        bundle_rendered = bundle.map(
-            lambda path: render_yaml(path, data, ops_test)
-        ).map(lambda data: generate_tmp_file(data, tmp_folder))
-
-        retcode, stdout, stderr = await deploy_bundle(ops_test, bundle_rendered)
-
-        assert retcode == 0, f"Deploy failed: {(stderr or stdout).strip()}"
-        logger.info(stdout)
-
-    await ops_test.model.wait_for_idle(
-        apps=["s3"], timeout=600, idle_period=30, status="blocked"
+    applications = await (
+        deploy_bundle_yaml(bundle, service_account, bucket, cos, ops_test)
+        if isinstance(bundle, Bundle)
+        else deploy_bundle_terraform(bundle, service_account, bucket, cos, ops_test)
     )
 
-    await set_s3_credentials(ops_test, credentials)
+    if "s3" in applications:
+        await ops_test.model.wait_for_idle(
+            apps=["s3"], timeout=600, idle_period=30, status="blocked"
+        )
 
-    if cos:
-        with ops_test.model_context(COS_ALIAS) as cos_model:
-            await cos_model.wait_for_idle(
-                apps=[
-                    "loki",
-                    "grafana",
-                    "prometheus",
-                    "catalogue",
-                    "traefik",
-                    "alertmanager",
-                ],
-                idle_period=60,
-                timeout=3600,
-                raise_on_error=False,
-            )
+        await set_s3_credentials(ops_test, credentials)
 
-    applications = list(render_yaml(bundle.main, data, ops_test)["applications"].keys())
-
-    print(applications)
+    logger.info(f"Applications: {applications}")
 
     await ops_test.model.wait_for_idle(
-        apps=applications, timeout=1800, idle_period=30, status="active"
+        apps=applications,
+        timeout=2500,
+        idle_period=30,
+        status="active",
+        raise_on_error=False,
     )
 
     for app in applications:
