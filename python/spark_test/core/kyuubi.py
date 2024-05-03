9825d805840b0c955924eae6d7ba732cfcfb83cc
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-05-03 02:11:09 +0200

    kyuubi integration tests

---
+++ python/spark_test/core/kyuubi.py
@@ -0,0 +1,187 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from pyhive.hive import Connection
+from contextlib import contextmanager
+
+from typing import Type
+
+TYPES = {
+    int: "int",
+    str: "string"
+}
+
+ITYPES = {
+    v: k for k, v in TYPES.items()
+}
+
+SchemaType = Type[int | str]
+
+class TableExists(Exception):
+    pass
+
+class TableNotFound(Exception):
+    pass
+
+class KyuubiClient:
+
+    def __init__(self, username: str, password: str,
+                 host: str, port: int = 10009):
+        self.username = username
+        self.password = password
+        self.host = host
+        self.port = port
+
+    @property
+    @contextmanager
+    def connection(self) -> Connection:
+        conn = Connection(
+            host=self.host,
+            port=self.port,
+            username=self.username,
+            password=self.password,
+            auth="CUSTOM"
+        )
+        yield conn
+        conn.close()
+
+    @property
+    def databases(self):
+        with (
+            self.connection as conn,
+            conn.cursor() as cursor
+        ):
+            cursor.execute("SHOW DATABASES;")
+            results = cursor.fetchall()
+            return [result[0] for result in results]
+
+    def get_database(self, name: str):
+        with (
+            self.connection as conn,
+            conn.cursor() as cursor
+        ):
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {name};")
+
+            return Database(name, self)
+
+class Database:
+
+    def __init__(self, name: str, client: KyuubiClient):
+        self.client = client
+        self.name = name
+
+    @property
+    def tables(self):
+        with (
+            self.client.connection as conn,
+            conn.cursor() as cursor
+        ):
+            cursor.execute(f"USE {self.name};")
+            cursor.execute("SHOW TABLES;")
+            results = cursor.fetchall()
+            return [result[1] for result in results]
+
+    def get_table(self, name: str):
+        if name not in self.tables:
+            raise TableNotFound()
+
+        with (
+            self.client.connection as conn,
+            conn.cursor() as cursor
+        ):
+            cursor.execute(f"DESCRIBE {self.name}.{name}")
+            schema = [
+                (str(value[0]), ITYPES[value[1]]) for value in cursor.fetchall()
+            ]
+            return Table(name, self, schema)
+
+    def drop(self):
+        with (
+            self.client.connection as conn,
+            conn.cursor() as cursor
+        ):
+            cursor.execute(f"DROP DATABASE {self.name};")
+
+    def create_table(self, name: str, schema: list[tuple[str, SchemaType]]):
+        if name in self.tables:
+            raise TableExists(name)
+
+        with (
+            self.client.connection as conn,
+            conn.cursor() as cursor
+        ):
+            schema_str = ", ".join(
+                f"{col_name} {TYPES[_type]}"
+                for col_name, _type in schema
+            )
+            cursor.execute(f"CREATE TABLE {self.name}.{name} ({schema_str});")
+
+            return Table(name, self, schema)
+
+
+class Table:
+
+    def __init__(
+            self, name: str, database: Database,
+            schema: list[tuple[str, SchemaType]]
+    ):
+        self.name = name
+        self.database = database
+        self.schema = schema
+
+    def validate(self, values: list[SchemaType]):
+        output = {}
+
+        for value, (col_name, _type) in zip(values, self.schema):
+            if not isinstance(value, _type):
+                raise TypeError(f"Column {col_name}: Expected {_type}, "
+                                f"found {type(value)}")
+            output[col_name] = value
+
+        return output
+
+    def drop(self):
+        with (
+            self.database.client.connection as conn,
+            conn.cursor() as cursor
+        ):
+            cursor.execute(f"DROP TABLE {self.database.name}.{self.name};")
+
+    def rows(self):
+        with (
+            self.database.client.connection as conn,
+            conn.cursor() as cursor
+        ):
+            cursor.execute(f"SELECT * FROM {self.database.name}.{self.name}")
+
+            for row in cursor.fetchall():
+                yield self.validate(row)
+
+    def parse_value(self, value: SchemaType):
+        match value:
+            case int():
+                return str(value)
+            case str():
+                return f'"{value}"'
+            case _:
+                raise TypeError(type(value))
+
+    def _parse_row(self, row: list[SchemaType]):
+
+        _ = self.validate(row)
+
+        return "(" + ','.join(
+            self.parse_value(value)
+            for value in row
+        ) + ")"
+
+    def _parse_rows(self, rows: list[list[SchemaType]]):
+        return ", ".join(self._parse_row(row) for row in rows)
+
+    def insert(self, *rows: list[SchemaType]):
+        with (
+            self.database.client.connection as conn,
+            conn.cursor() as cursor
+        ):
+            cursor.execute(f"INSERT INTO {self.database.name}.{self.name} "
+                           f"VALUES {self._parse_rows(list(rows))};")
---
+++ python/tests/integration/test_kyuubi.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+
+import pytest
+from pytest_operator.plugin import OpsTest
+
+from spark_test.fixtures.k8s import envs, interface, kubeconfig, namespace
+from spark_test.fixtures.s3 import bucket, credentials
+from spark_test.fixtures.service_account import registry, service_account
+from .helpers import (
+    Bundle,
+    deploy_bundle_terraform,
+    deploy_bundle_yaml,
+    set_s3_credentials,
+    get_kyuubi_credentials
+)
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="module")
+def bucket_name():
+    return "spark-bucket"
+
+
+@pytest.fixture(scope="module")
+def namespace_name(ops_test: OpsTest):
+    return ops_test.model_name
+
+
+@pytest.fixture(scope="module")
+def namespace(namespace_name):
+    return namespace_name
+
+
+@pytest.mark.abort_on_fail
+@pytest.mark.asyncio
+async def test_deploy_bundle(
+        ops_test: OpsTest, credentials, bucket, service_account,
+        bundle, cos
+):
+    """Deploy the bundle."""
+
+    applications = await (
+        deploy_bundle_yaml(bundle, service_account, bucket, cos, ops_test)
+        if isinstance(bundle, Bundle)
+        else deploy_bundle_terraform(bundle, service_account, bucket, cos,
+                                     ops_test)
+    )
+
+    if "s3" in applications:
+        await ops_test.model.wait_for_idle(
+            apps=["s3"], timeout=600, idle_period=30, status="blocked"
+        )
+
+        await set_s3_credentials(ops_test, credentials)
+
+    logger.info(f"Applications: {applications}")
+
+    await ops_test.model.wait_for_idle(
+        apps=applications,
+        timeout=2500,
+        idle_period=30,
+        status="active",
+        raise_on_error=False,
+    )
+
+    for app in applications:
+        assert ops_test.model.applications[app].status == "active"
+
+
+@pytest.mark.abort_on_fail
+@pytest.mark.asyncio
+async def test_jdbc_endpoint(ops_test: OpsTest, service_account):
+    """Use JDBC Kyuubi Endpoint."""
+
+    credentials = await get_kyuubi_credentials(ops_test, "kyuubi")
+
+    from spark_test.core.kyuubi import KyuubiClient
+
+    client = KyuubiClient(**credentials)
+
+    db_name, table_name = "spark_test", "my_table"
+
+    # Create a database
+    db = client.get_database(db_name)
+
+    assert db_name in client.databases
+
+    table = db.create_table(
+        table_name,
+        [("name", str), ("country", str), ("year_birth", int)]
+    )
+
+    assert table_name in db.tables
+
+    table.insert(
+        ("messi", "argentina", 1987),
+        ("sinner", "italy", 2002),
+        ("jordan", "usa", 1963)
+    )
+
+    assert len(list(table.rows())) == 3
