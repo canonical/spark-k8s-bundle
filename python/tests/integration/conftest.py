680adb9729e7c3a54fb4163009b67a014d38764f
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2024-09-09 06:43:31 +0000

    Reduce constraints to 250M

--- python/tests/integration/conftest.py
+++ python/tests/integration/conftest.py
@@ -94,7 +94,7 @@ def ops_test(ops_test: OpsTest) -> OpsTest:
             "set-model-constraints",
             "--model",
             ops_test.model.info.name,
-            "mem=500M",
+            "mem=250M",
         ],
         check=True,
     )
