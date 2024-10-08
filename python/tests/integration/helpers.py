75e5709df4bf02952bee2bedb62f592c5bb5190e
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2024-10-08 10:35:07 +0000

    INcrease memory constraint to 750M

--- python/tests/integration/helpers.py
+++ python/tests/integration/helpers.py
@@ -137,7 +137,7 @@ async def set_memory_constraints(ops_test, model_name):
         "set-model-constraints",
         "--model",
         model_name,
-        "mem=500M",
+        "mem=750M",
     ]
     retcode, stdout, stderr = await ops_test.juju(*model_constraints_command)
     assert retcode == 0
