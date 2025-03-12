ea4811c2c3d604852a735d30260c452d37747a63
Alex Batisse <alex.batisse@canonical.com>
2025-03-12 09:33:08 +0100

    [DPE-6711] Re-enable TLS on bundles (#93)

--- python/tests/integration/test_kyuubi.py
+++ python/tests/integration/test_kyuubi.py
@@ -3,6 +3,8 @@
 # See LICENSE file for licensing details.
 
 import asyncio
+import ast
+import json
 import logging
 
 import psycopg2
@@ -55,7 +57,7 @@ async def test_authentication_is_enforced(ops_test):
     credentials["password"] = "something-random"
 
     with pytest.raises(Exception) as e:
-        client = KyuubiClient(**credentials)
+        client = KyuubiClient(**credentials, use_ssl=True)
         client.get_database("spark_test")
 
         assert "Error validating the login" in str(e)
@@ -66,7 +68,18 @@ async def test_jdbc_endpoint(ops_test: OpsTest):
     """Test that JDBC connection in Kyuubi works out of the box in bundle."""
 
     credentials = await get_kyuubi_credentials(ops_test, "kyuubi")
-    client = KyuubiClient(**credentials)
+
+    logger.info("Get certificate from self-signed-certificates operator")
+    self_signed_certificate_unit = ops_test.model.applications["certificates"].units[0]
+    action = await self_signed_certificate_unit.run_action(
+        action_name="get-issued-certificates",
+    )
+    result = await action.wait()
+    items = ast.literal_eval(result.results.get("certificates"))
+    certificates = json.loads(items[0])
+    ca_cert = certificates["ca"]
+
+    client = KyuubiClient(**credentials, use_ssl=True, ca_cert=ca_cert)
 
     db_name, table_name = "spark_test", "my_table"
 
@@ -200,7 +213,7 @@ async def test_kyuubi_metrics_in_cos(ops_test: OpsTest, cos):
 @pytest.mark.abort_on_fail
 async def test_drop_table_if_exists(ops_test: OpsTest):
     credentials = await get_kyuubi_credentials(ops_test, "kyuubi")
-    client = KyuubiClient(**credentials)
+    client = KyuubiClient(**credentials, use_ssl=True)
 
     db_name, table_name = "spark_test", "my_table"
 
