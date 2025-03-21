41a7c08f09e9a0ab9eb57100b5656484f49df1a1
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-03-21 14:12:30 +0545

    Delete objects in GCS copmatible way

--- python/spark_test/core/s3.py
+++ python/spark_test/core/s3.py
@@ -164,8 +164,8 @@ class Bucket(ObjectStorageUnit):
     def cleanup(self) -> bool:
         """Cleanup objects from bucket."""
         try:
-            objs = [{"Key": x["Key"]} for x in self.list_objects()]
-            self.s3.delete_objects(Bucket=self.bucket_name, Delete={"Objects": objs})
+            for obj in self.list_objects():
+                self.s3.delete_object(Bucket=self.bucket_name, Key=obj["Key"])
         except Exception:
             logger.exception("Issue while deleting file")
             return False
