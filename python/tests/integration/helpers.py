24d957b081289543d08c1ac22aa8e39f4e3289c4
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-03-21 17:32:18 +0545

    Run get-jdbc-endpoint only in leader unit

--- python/tests/integration/helpers.py
+++ python/tests/integration/helpers.py
@@ -130,7 +130,8 @@ async def get_kyuubi_credentials(
 async def fetch_jdbc_endpoint(ops_test):
     """Return the JDBC endpoint for clients to connect to Kyuubi server."""
     logger.info("Running action 'get-jdbc-endpoint' on kyuubi-k8s unit...")
-    kyuubi_unit = ops_test.model.applications["kyuubi"].units[0]
+    leader_unit_number = await get_leader_unit_number(ops_test, "kyuubi")
+    kyuubi_unit = ops_test.model.applications["kyuubi"].units[leader_unit_number]
     action = await kyuubi_unit.run_action(
         action_name="get-jdbc-endpoint",
     )
