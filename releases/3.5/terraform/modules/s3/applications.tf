72ca09193dde748a2cda9a2781e4a8b01ddfd844
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-06-05 18:22:29 +0200

    extending modifications to 3.5

--- releases/3.5/terraform/modules/s3/applications.tf
+++ releases/3.5/terraform/modules/s3/applications.tf
@@ -7,7 +7,7 @@ resource "juju_application" "s3" {
   charm {
     name     = "s3-integrator"
     channel  = "latest/stable"
-    revision = 77
+    revision = var.s3_integrator_revision
   }
   config = {
     path     = "spark-events"
