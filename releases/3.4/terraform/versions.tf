a194625fe6f4204142ad8d7e216125bfa8b9603b
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-06-05 08:49:22 +0200

    fix linting

--- releases/3.4/terraform/versions.tf
+++ releases/3.4/terraform/versions.tf
@@ -26,22 +26,22 @@ locals {
     prometheus_scrape_config = 64
   }
   images = {
-    spark_history_server  = {
-      spark-history-server-image="ghcr.io/canonical/charmed-spark@sha256:1d9949dc7266d814e6483f8d9ffafeff32f66bb9939e0ab29ccfd9d5003a583a"
+    spark_history_server = {
+      spark-history-server-image = "ghcr.io/canonical/charmed-spark@sha256:1d9949dc7266d814e6483f8d9ffafeff32f66bb9939e0ab29ccfd9d5003a583a"
     } # 3.4.2
     spark_integration_hub = {
       integration-hub-image = 5
     }
     kyuubi                = {
-      kyuubi-image = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:29c84e1693ce7b5e6cf4fcb84570a79357f9bc1e66bce59d2e0031f1314699e5" 
+      kyuubi-image = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:29c84e1693ce7b5e6cf4fcb84570a79357f9bc1e66bce59d2e0031f1314699e5"
     } # 3.4.4-1.10.1-22.04_edge 2025-05-06
     kyuubi_users          = {
       postgresql-image = 159
     }
-    metastore             = {
+    metastore = {
       postgresql-image = 159
     }
-    zookeeper             = {
+    zookeeper = {
       zookeeper-image = 34
     }
     # grafana_agent       = ...
