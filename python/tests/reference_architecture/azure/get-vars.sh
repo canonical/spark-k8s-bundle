9755da9886feb33a76bf00dba779930062fa4868
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-06-05 18:47:42 +0200

    remove reference architecture stuff

--- python/tests/reference_architecture/azure/get-vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export TF_VAR_JUJU_CONTROLLER_IPS=$(cat ~/.local/share/juju/controllers.yaml | yq .controllers.micro.api-endpoints | sed 's/\[\(.*\)\]/\1/g' | sed "s/'//g")
-export TF_VAR_JUJU_USERNAME=$(cat ~/.local/share/juju/accounts.yaml| yq .controllers.micro.user)
-export TF_VAR_JUJU_PASSWORD=$(cat ~/.local/share/juju/accounts.yaml| yq .controllers.micro.password)
-export TF_VAR_JUJU_CA_CERTIFICATE=$(cat ~/.local/share/juju/controllers.yaml | yq .controllers.micro.ca-cert | base64)
-export TF_VAR_K8S_CLOUD="microk8s"
-export TF_VAR_K8S_CREDENTIAL="microk8s"
