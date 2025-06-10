8e122cc0c3f6cfcabb35bde69ac428471639cc2f
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-06-10 18:17:11 +0200

    update bash script

--- python/tests/reference_architecture/azure/get-vars.sh
+++ python/tests/reference_architecture/azure/get-vars.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export TF_VAR_JUJU_CONTROLLER_IPS=$(cat ~/.local/share/juju/controllers.yaml | yq .controllers.micro.api-endpoints | sed 's/\[\(.*\)\]/\1/g' | sed "s/'//g")
-export TF_VAR_JUJU_USERNAME=$(cat ~/.local/share/juju/accounts.yaml| yq .controllers.micro.user)
-export TF_VAR_JUJU_PASSWORD=$(cat ~/.local/share/juju/accounts.yaml| yq .controllers.micro.password)
-export TF_VAR_JUJU_CA_CERTIFICATE=$(cat ~/.local/share/juju/controllers.yaml | yq .controllers.micro.ca-cert | base64)
-export TF_VAR_K8S_CLOUD="microk8s"
-export TF_VAR_K8S_CREDENTIAL="microk8s"
+CURRENT_CONTROLLER=$(cat ~/.local/share/juju/controllers.yaml | yq .current-controller)
+
+export TF_VAR_JUJU_CONTROLLER_IPS=$(cat ~/.local/share/juju/controllers.yaml | yq ".controllers.${CURRENT_CONTROLLER}.api-endpoints" | sed 's/\[\(.*\)\]/\1/g' | sed "s/'//g")
+export TF_VAR_JUJU_USERNAME=$(cat ~/.local/share/juju/accounts.yaml| yq ".controllers.${CURRENT_CONTROLLER}.user")
+export TF_VAR_JUJU_PASSWORD=$(cat ~/.local/share/juju/accounts.yaml| yq ".controllers.${CURRENT_CONTROLLER}.password")
+export TF_VAR_JUJU_CA_CERTIFICATE=$(cat ~/.local/share/juju/controllers.yaml | yq ".controllers.${CURRENT_CONTROLLER}.ca-cert" | base64)
+export TF_VAR_K8S_CLOUD=$(cat ~/.local/share/juju/controllers.yaml | yq ".controllers.${CURRENT_CONTROLLER}.cloud")
+export TF_VAR_K8S_CREDENTIAL=$TF_VAR_K8S_CLOUD
