4cbb3329112e0c2c0db39f949de0ebc4931c6f6b
Alex Batisse <alex.batisse@canonical.com>
2025-06-10 13:50:21 +0200

    chore: Pin s3-integrator to stable and update module tables

--- releases/3.4/terraform/main.tf
+++ releases/3.4/terraform/main.tf
@@ -36,7 +36,7 @@ module "ssc" {
   source      = "git::https://github.com/canonical/self-signed-certificates-operator//terraform"
   model       = var.model
   app_name    = "certificates"
-  channel     = "latest/stable"
+  channel     = "1/stable"
   revision    = 163
   constraints = "arch=amd64"
   base        = "ubuntu@22.04"
