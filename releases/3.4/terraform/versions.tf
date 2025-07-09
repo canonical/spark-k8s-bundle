0779d2def0a89909a85ceb407daf402f999697d7
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-07-09 15:29:21 +0545

    Test the Spark bundle with Kyuubi rev 97

--- releases/3.4/terraform/versions.tf
+++ releases/3.4/terraform/versions.tf
@@ -14,7 +14,7 @@ locals {
   revisions = {
     history_server    = 40
     integration_hub   = 49
-    kyuubi            = 95
+    kyuubi            = 97
     kyuubi_users      = 281
     metastore         = 281
     zookeeper         = 75
