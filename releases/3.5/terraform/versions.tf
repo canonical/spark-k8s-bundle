6b4f756fda430aaadb899d9648cb59a2d419e33b
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-07-11 21:03:34 +0545

    Move juju pin to versions.tf

--- releases/3.5/terraform/versions.tf
+++ releases/3.5/terraform/versions.tf
@@ -4,8 +4,11 @@
 terraform {
   required_providers {
     juju = {
+      # Pin the version to <0.20 because 0.20.0 introduces some breaking changes
+      # in `juju_offer` resource.
+      # See: https://github.com/juju/terraform-provider-juju/issues/780
+      version = ">=0.16.0, <0.20"
       source  = "juju/juju"
-      version = ">=0.16.0"
     }
   }
 }
