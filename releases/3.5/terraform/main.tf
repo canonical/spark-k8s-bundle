145a0d1fb029e2c740a9251f9c62b4b28219c97f
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-07-18 09:51:05 +0545

    Add new variables in terraform module

--- releases/3.5/terraform/main.tf
+++ releases/3.5/terraform/main.tf
@@ -32,16 +32,21 @@ module "ssc" {
 }
 
 module "spark" {
-  depends_on                = [juju_model.spark, module.ssc]
-  source                    = "./modules/spark"
-  model                     = var.model
-  kyuubi_user               = var.kyuubi_user
-  kyuubi_profile            = var.kyuubi_profile
-  admin_password            = var.admin_password
-  tls_private_key           = var.tls_private_key
-  zookeeper_units           = var.zookeeper_units
-  tls_app_name              = module.ssc.app_name
-  tls_certificates_endpoint = module.ssc.provides.certificates
+  depends_on                      = [juju_model.spark, module.ssc]
+  source                          = "./modules/spark"
+  model                           = var.model
+  kyuubi_user                     = var.kyuubi_user
+  kyuubi_profile                  = var.kyuubi_profile
+  admin_password                  = var.admin_password
+  tls_private_key                 = var.tls_private_key
+  enable_dynamic_allocation       = var.enable_dynamic_allocation
+  k8s_node_selectors              = var.k8s_node_selectors
+  loadbalancer_extra_annotations  = var.loadbalancer_extra_annotations
+  driver_pod_template             = var.driver_pod_template
+  executor_pod_template           = var.executor_pod_template
+  zookeeper_units                 = var.zookeeper_units
+  tls_app_name                    = module.ssc.app_name
+  tls_certificates_endpoint       = module.ssc.provides.certificates
 
   history_server_revision  = var.history_server_revision != null ? var.history_server_revision : local.revisions.history_server
   history_server_image     = var.history_server_image != null ? var.history_server_image : local.images.history_server
