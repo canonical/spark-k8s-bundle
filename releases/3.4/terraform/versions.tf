4cdea3c9cc40490f35cccf4f2420b9e97e2d5b35
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-07-28 20:40:26 +0200

    update revision to latest release artefacts

--- releases/3.4/terraform/versions.tf
+++ releases/3.4/terraform/versions.tf
@@ -12,13 +12,13 @@ terraform {
 
 locals {
   revisions = {
-    history_server  = 44
-    integration_hub = 64
-    kyuubi          = 104
+    history_server  = 47
+    integration_hub = 66
+    kyuubi          = 109
     kyuubi_users    = 495
     metastore       = 495
     zookeeper       = 78
-    data_integrator = 181
+    data_integrator = 179
     s3              = 145
     azure_storage   = 15
     grafana_agent   = 121
@@ -30,14 +30,14 @@ locals {
   }
   images = {
     history_server = {
-      spark-history-server-image = "ghcr.io/canonical/charmed-spark@sha256:99e8494070af297e9cfb6965e5216abef33539ae36fde34cbcbd2d7acb433e60"
-    } # rev20 3.4 -- 30 Jan 2025
+      spark-history-server-image = "ghcr.io/canonical/charmed-spark@sha256:f1f944369108c0b0112212fb0242f3c314dfad362926c234857029f13c5de2c0"
+    } # rev21, spark-version: 3.5.5 revision: c9fe483160d8aad5d23260af98c18e2653720c8e
     integration_hub = {
-      integration-hub-image = 5
-    }
+      integration-hub-image = "ghcr.io/canonical/spark-integration-hub@sha256:fa5e73d6339b2eb137b5917771caa62bd6605284b8dfab3dafb7d6026a9a3b1a"
+    } # rev
     kyuubi = {
-      kyuubi-image = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:153eaf8be341dcea2c91277cbc2a69a1c9c48d3f7847151898ab2e5a81753ec5"
-    } # rev8 3.4 -- 16 Jun 2025 
+      kyuubi-image = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:38f35ce47b84b8370da9a371edaf83ca9911347a1efd6eb0cfbc8128f051b1e4"
+    } # rev9, spark-3.4.4, kyuubi 1.10.2 release date 25/07/25
     kyuubi_users = {
       postgresql-image = 165
     }
--- releases/3.5/terraform/versions.tf
+++ releases/3.5/terraform/versions.tf
@@ -12,13 +12,13 @@ terraform {
 
 locals {
   revisions = {
-    history_server  = 44
-    integration_hub = 64
-    kyuubi          = 104
+    history_server  = 47
+    integration_hub = 66
+    kyuubi          = 111
     kyuubi_users    = 495
     metastore       = 495
     zookeeper       = 78
-    data_integrator = 181
+    data_integrator = 179
     s3              = 145
     azure_storage   = 15
     grafana_agent   = 121
@@ -30,14 +30,14 @@ locals {
   }
   images = {
     history_server = {
-      spark-history-server-image = "ghcr.io/canonical/charmed-spark@sha256:857f69f583fd8ee6238d1e556f749e29847ec83010c5d53f4a66388b00940da5"
-    } # 3.5.4-22.04_edge 2025-05-02
+      spark-history-server-image = "ghcr.io/canonical/charmed-spark@sha256:f1f944369108c0b0112212fb0242f3c314dfad362926c234857029f13c5de2c0"
+    } # rev21, spark-version: 3.5.5 revision: c9fe483160d8aad5d23260af98c18e2653720c8e
     integration_hub = {
-      integration-hub-image = 5
-    }
+      integration-hub-image = "ghcr.io/canonical/spark-integration-hub@sha256:fa5e73d6339b2eb137b5917771caa62bd6605284b8dfab3dafb7d6026a9a3b1a"
+    } # rev
     kyuubi = {
-      kyuubi-image = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:b91f24a14889bc4953560c5d54d1cb7c00d5bf238607dde70b02e5ce17190b5f"
-    } # 3.5.4-1.9.0-22.04_edge 2025-05-02
+      kyuubi-image = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:e2029c6976fc5b9ee2865eced632ca42ce554039d9832af20dfa3e63113e00f7"
+    } # rev11, spark-3.5.5, kyuubi 1.10.2 release date 28/07/25
     kyuubi_users = {
       postgresql-image = 165
     }
