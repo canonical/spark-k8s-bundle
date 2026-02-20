544b70e8290b1a94808a20943c17cc5f7d35e06a
Alex Batisse <alex.batisse@canonical.com>
2026-02-20 14:58:22 +0100

    Remove deleted modules from output

--- releases/3.5/terraform-arm/outputs.tf
+++ releases/3.5/terraform-arm/outputs.tf
@@ -5,7 +5,7 @@ output "charms" {
   description = "The name of the charms which are part of the deployment."
   value = merge(
     concat(
-      module.spark[*].charms, module.azure_storage[*].charms, module.s3[*].charms, module.bundled_cos[*].charms, module.observability[*].charms
+      module.spark[*].charms, module.s3[*].charms
     )...
   )
 }
