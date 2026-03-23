b60836c1878f1b7d06651918f0953db8b565c519
Enrico Deusebio <enrico.deusebio@canonical.com>
2026-03-23 10:16:49 +0100

    update spark-integration-hub-k8s references

--- releases/3.4/terraform/versions.tf
+++ releases/3.4/terraform/versions.tf
@@ -13,7 +13,7 @@ terraform {
 locals {
   revisions = {
     history_server  = 96
-    integration_hub = 113
+    integration_hub = 119
     kyuubi          = 149
     kyuubi_users    = 495
     metastore       = 495
@@ -34,8 +34,8 @@ locals {
       spark-history-server-image = "ghcr.io/canonical/charmed-spark@sha256:f7f387a76ba2f3b6cfc34a35f36c0f4ae85bfa78c8c9d4bfa06ba86401b95d70"
     } # rev24,  spark-version: 3.5.7, release date 16/03/2026 
     integration_hub = {
-      integration-hub-image = "ghcr.io/canonical/spark-integration-hub@sha256:c04d2d95874612883f6ead105d5f1dd74230370b23a08ea81175df81ba2e06fa"
-    } # rev11, release date 2026-03-02
+      integration-hub-image = "ghcr.io/canonical/spark-integration-hub@sha256:a0439da3e6a9433a0db9501c8ea7b28ba3652af9a9ec22fcc2ad1b0197d48134"
+    } # rev13, release date 19/03/2026
     kyuubi = {
       kyuubi-image = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:60e81dd2e9be50d4a857ce987935341c8518c784c7de36e814fc41dd3451d58f"
     } # rev19, spark-version: 3.4.4, kyuubi-version: 1.10.3, release date 17/03/2026
--- releases/3.5/terraform/versions.tf
+++ releases/3.5/terraform/versions.tf
@@ -13,7 +13,7 @@ terraform {
 locals {
   revisions = {
     history_server  = 96
-    integration_hub = 113
+    integration_hub = 119
     kyuubi          = 146
     kyuubi_users    = 495
     metastore       = 495
@@ -33,9 +33,8 @@ locals {
     history_server = {
       spark-history-server-image = "ghcr.io/canonical/charmed-spark@sha256:f7f387a76ba2f3b6cfc34a35f36c0f4ae85bfa78c8c9d4bfa06ba86401b95d70"
     } # rev24,  spark-version: 3.5.7, release date 16/03/2026 
-    integration_hub = {
-      integration-hub-image = "ghcr.io/canonical/spark-integration-hub@sha256:c04d2d95874612883f6ead105d5f1dd74230370b23a08ea81175df81ba2e06fa"
-    } # rev11, release date 2026-03-02
+    integration-hub-image = "ghcr.io/canonical/spark-integration-hub@sha256:a0439da3e6a9433a0db9501c8ea7b28ba3652af9a9ec22fcc2ad1b0197d48134"
+    } # rev13, release date 19/03/2026
     kyuubi = {
       kyuubi-image = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:21beea406001d161538b552cef2b47b86423bd899139ada263c1ef665fd89fff"
     } # rev18, spark-version: 3.5.7, kyuubi-version: 1.10.3, release date 16/03/2026
