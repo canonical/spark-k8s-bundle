23ccd6ff765d0e4be05b527efcbc9f9b09825075
Alex Batisse <alex.batisse@canonical.com>
2026-04-08 15:21:11 +0200

    Re-align 3.5 revisions

--- terraform/products/charmed-spark-3.5/locals.tf
+++ terraform/products/charmed-spark-3.5/locals.tf
@@ -5,7 +5,7 @@ locals {
   revisions = {
     history_server  = 96
     integration_hub = 119
-    kyuubi          = 149
+    kyuubi          = 146
     kyuubi_users    = 774
     metastore       = 774
     zookeeper       = 78
@@ -26,8 +26,8 @@ locals {
     # rev24,  spark-version: 3.5.7, release date 16/03/2026 
     integration_hub = "ghcr.io/canonical/spark-integration-hub@sha256:a0439da3e6a9433a0db9501c8ea7b28ba3652af9a9ec22fcc2ad1b0197d48134"
     # rev13, release date 19/03/2026
-    kyuubi = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:60e81dd2e9be50d4a857ce987935341c8518c784c7de36e814fc41dd3451d58f"
-    # rev19, spark-version: 3.4.4, kyuubi-version: 1.10.3, release date 17/03/2026
+    kyuubi = "ghcr.io/canonical/charmed-spark-kyuubi@sha256:21beea406001d161538b552cef2b47b86423bd899139ada263c1ef665fd89fff"
+    # rev19, spark-version: 3.5.7, kyuubi-version: 1.10.3, release date 16/03/2026
     kyuubi_users = "184"
     metastore    = "184"
     zookeeper    = "34"
