625c225413573bf74ed9b7ec53f713a295567bae
Alex Batisse <alex.batisse@canonical.com>
2026-04-15 14:14:31 +0200

    Remove spark component module locals.tf

--- terraform/components/spark/applications.tf
+++ terraform/components/spark/applications.tf
@@ -8,17 +8,13 @@ resource "juju_application" "history_server" {
   charm {
     name     = "spark-history-server-k8s"
     channel  = var.history_server.channel
-    revision = var.history_server.revision != null ? var.history_server.revision : local.revisions.history_server
+    revision = var.history_server.revision
   }
 
   config      = var.history_server.config
   constraints = var.history_server.constraints
-  resources = merge({
-    spark-history-server-image = local.images.history_server
-    },
-    var.history_server.resources == null ? {} : var.history_server.resources
-  )
-  units = var.history_server.units
+  resources   = var.history_server.resources
+  units       = var.history_server.units
 }
 
 resource "juju_application" "integration_hub" {
@@ -28,18 +24,14 @@ resource "juju_application" "integration_hub" {
   charm {
     name     = "spark-integration-hub-k8s"
     channel  = var.integration_hub.channel
-    revision = var.integration_hub.revision != null ? var.integration_hub.revision : local.revisions.integration_hub
+    revision = var.integration_hub.revision
   }
 
   config      = var.integration_hub.config
   constraints = var.integration_hub.constraints
-  resources = merge({
-    integration-hub-image = local.images.integration_hub
-    },
-    var.integration_hub.resources == null ? {} : var.integration_hub.resources
-  )
-  trust = true
-  units = var.integration_hub.units
+  resources   = var.integration_hub.resources
+  trust       = true
+  units       = var.integration_hub.units
 }
 
 resource "juju_application" "kyuubi" {
@@ -49,16 +41,12 @@ resource "juju_application" "kyuubi" {
   charm {
     name     = "kyuubi-k8s"
     channel  = var.kyuubi.channel
-    revision = var.kyuubi.revision != null ? var.kyuubi.revision : local.revisions.kyuubi
+    revision = var.kyuubi.revision
   }
 
   config      = var.kyuubi.config
   constraints = var.kyuubi.constraints
-  resources = merge({
-    kyuubi-image = local.images.kyuubi
-    },
-    var.kyuubi.resources == null ? {} : var.kyuubi.resources
-  )
-  trust = true
-  units = var.kyuubi.units
+  resources   = var.kyuubi.resources
+  trust       = true
+  units       = var.kyuubi.units
 }
