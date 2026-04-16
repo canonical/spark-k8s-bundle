463ea69c5f66eb73d2b3d9589a6b74192d14b720
Alex Batisse <alex.batisse@canonical.com>
2026-04-16 15:56:40 +0200

    [PRA-234] Terraform refactor following CC008 (#208)

--- python/tests/__init__.py
+++ python/tests/__init__.py
@@ -7,4 +7,5 @@ from pathlib import Path
 
 IE_TEST_DIR = Path(os.path.dirname(__file__))
 
-RELEASE_DIR = IE_TEST_DIR / ".." / ".." / "releases"
+TERRAFORM_DIR = IE_TEST_DIR / ".." / ".." / "terraform"
+PRODUCTS_DIR = TERRAFORM_DIR / "products"
--- releases/3.4/yaml/README.md
+++
@@ -1,5 +0,0 @@
-# Charmed Apache Spark Kubernetes Bundle
-
-## A fast and fault-tolerant, real-time event streaming platform!
-
-...
