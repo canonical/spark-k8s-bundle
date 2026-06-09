84d9e3a0cfbe3a2c4a9e790bede3246699d27f17
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2026-06-09 12:35:54 +0545

    Fix lint and formatting issues

--- python/tests/integration/conftest.py
+++ python/tests/integration/conftest.py
@@ -506,12 +506,8 @@ def spark_bundle(
                 if not unpinned_revisions
                 else {}
             ),
-            **(
-                # TODO: Remove this once we use stable risk for Spark 4
-                {"spark_risk": "edge"}
-                if short_version not in {"3.4", "3.5"}
-                else {}
-            ),
+            # TODO: Remove this once we use stable risk for Spark 4
+            **({"spark_risk": "edge"} if short_version not in {"3.4", "3.5"} else {}),
         }
     # Merge external Terraform variables
     base_vars.update(tfvars)
