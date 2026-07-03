94db25bdef44455f6a8bcb0c8d013c0a6bbfed1a
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2026-07-03 08:46:35 +0545

    Fix formatting issues

--- terraform/products/charmed-spark/terraform.tf
+++ terraform/products/charmed-spark/terraform.tf
@@ -6,7 +6,8 @@ terraform {
 
   required_providers {
     juju = {
-      source  = "juju/juju"
+      source = "juju/juju"
+      # TODO: Unpin juju<1.5.5 when https://github.com/juju/terraform-provider-juju/pull/1298 gets merged and released
       version = ">=1.0.0,<1.5.5"
     }
   }
